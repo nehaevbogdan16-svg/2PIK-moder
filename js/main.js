586d6154b39799ddf2008792eb707be6af170733
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2026-06-15 15:10:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-console.log("2PIK Official Website Loaded");
+const news = [
+
+    {
+
+        title: "Открытие официального сайта",
+
+        date: "15.06.2026"
+
+    },
+
+    {
+
+        title: "Набор новых участников",
+
+        date: "15.06.2026"
+
+    }
+
+];
+
+const container =
+
+    document.getElementById(
+
+        "news-container"
+
+    );
+
+news.forEach(item => {
+
+    const div =
+
+        document.createElement("div");
+
+    div.className =
+
+        "news-item";
+
+    div.innerHTML = `
+
+        <h3>${item.title}</h3>
+
+        <p>${item.date}</p>
+
+    `;
+
+    container.appendChild(div);
+
+});
